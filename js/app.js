757212eb260c929b7a63376762dae2db1195e9e6
badshab454 <badshab454@gmail.com>
2022-11-18 23:27:29 +0600

    switch statements

--- js/app.js
+++ js/app.js
@@ -5,3 +5,14 @@ if (result > 40) {
     console.log("Unfortunately you did not pass.");
 }
 
+var place = 'first';
+
+switch (place) {
+    case 'first':
+        console.log('Gold');    
+        break;
+
+    default:
+        console.log('no medal');
+        break;
+}
