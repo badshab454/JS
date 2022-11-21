c84fdabb16246c9479c3b2415a112736ce33fbc1
badshab454 <badshab454@gmail.com>
2022-11-21 23:27:58 +0600

    switch statements

--- js/app.js
+++ js/app.js
@@ -9,4 +9,20 @@
 //         console.log(i);
 //     }
 // }
-
+for (i=1; i<=10; i++) {
+    switch (i) {
+        case 1:
+            console.log("Gold medal");
+            break;
+        case 2:
+            console.log("Silver Medal");    
+            break;
+        case 3:
+            console.log("Bronze medal");    
+            break;
+    
+        default:
+            console.log(i);
+            break;
+    }
+}
