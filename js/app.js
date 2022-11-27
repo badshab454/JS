154606dd4a72d41970ed14d6a9ecdfa37984cb4f
Badsha Hossain <badshab454@gmail.com>
2022-11-27 19:59:10 +0600

    letterFinder

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 function letterFinder(word, match) {
     for(i = 0; i < word.length; i++) {
-        
+        if(word[i] == match) {
+            console.log('Found the', match, 'at', i);
+        }
     }
 }
