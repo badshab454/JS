26fc6871185e9873b09833014932f062c904ce28
Badsha Hossain <badshab454@gmail.com>
2022-12-14 12:01:39 +0600

    js modules

--- js/app.js
+++
@@ -1,4 +0,0 @@
-const fruits = ['apple', 'pear', 'plum'];
-const berries = ['blueberry', 'strawberry'];
-const fruitsAndBerries = [...fruits, ...berries] // concatenate
-console.log(fruitsAndBerries); // outputs a single array
