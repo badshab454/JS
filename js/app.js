c796ee482f3455324c2bf40d0e7ff3cc50dac83a
Badsha Hossain <badshab454@gmail.com>
2022-12-28 00:39:56 +0600

    spread operator

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 //REST & SPREAD OPERATOR
 //EXAMPLE REST
 
+//rest
+
 function concatNumbers (a,b,c,...other) {
     console.log(arguments);
     return a+b+c;
@@ -10,3 +12,13 @@ function concatNumbers (a,b,c,...other) {
 var sum = concatNumbers(1,2,3,4,5,6,7,8);
 
 console.log(sum);
+
+//spread
+
+var names = ['badsha', 'hossain', 'babu'];
+
+function getNames(name1,name2,name3) {
+    console.log(name1,name2,name3);
+}
+
+console.log(names[0],names[1],names[2]);
