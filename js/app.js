1f2d3e5a3493c932df6cc32e60efc1f0b99127e3
Badsha Hossain <badshab454@gmail.com>
2023-02-22 23:46:29 +0600

    quokka

--- js/app.js
+++ js/app.js
@@ -1,24 +1,3 @@
-//ES6
-//REST & SPREAD OPERATOR
-//EXAMPLE REST
+// map method
 
-//rest
-
-function concatNumbers (a,b,c,...other) {
-    console.log(arguments);
-    return a+b+c;
-} 
-
-var sum = concatNumbers(1,2,3,4,5,6,7,8);
-
-console.log(sum);
-
-//spread
-
-var names = ['badsha', 'hossain', 'babu'];
-
-function getNames(name1,name2,name3) {
-    console.log(name1,name2,name3);
-}
-
-console.log(names[0],names[1],names[2]);
+console.log("hello world");
